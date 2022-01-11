8c2b98255c8367a6a1a6f24257ab6c9b6d453cb8
Sergey <levashov04sergey@gmail.com>
2022-01-11 21:32:05 +0200

    create App header, component BudgetCard, styling by bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import {Stack, Button} from "react-bootstrap";
+import {} from "bootstrap";
+
 function App() {
   return (
-    null
+      <Container className="my-4">
+        <Stack direction="horizontal" gap="2" className="mb-4">
+            <h1 className="me-auto">Budgets</h1>
+            <Button variant="primary">Add Budget</Button>
+            <Button variant="outline-primary">Add Expense</Button>
+        </Stack>
+        <div style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+            gap: "1rem",
+            alignItems: "flex-start"
+        }}>
+            <BudgetCard></BudgetCard>
+        </div>
+      </Container>
   );
 }
 
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Card} from "react-bootstrap";
+
+const BudgetCard = ({name, amount, max}) => {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                    <div>{name}</div>
+                    <div>{amount} / {max}</div>
+                </Card.Title>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default BudgetCard;
